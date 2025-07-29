549edc659512488bbab23431d80750b136879b20
FPabloA <pabloayala@protonmail.com>
2025-07-29 13:02:22 -0500

    added the extra naming and restart functionalities

--- javascript.js
+++ javascript.js
@@ -1,60 +1,106 @@
 const gameWrapper = document.querySelector('#game-display');
 const gameHeader = document.querySelector('#game-header');
 
-function createGameBoard () {
+function createGameBoard (name1, name2) {
     let boardList = ["", "", "","", "", "","", "", ""];
     let turn = 0;
     let playerTurn = "X";
+    const xPlayer = name1;
+    const oPlayer = name2;
 
     const nextTurn = function () {
+        console.log(this);
         turn++;
         if (this.playerTurn == "X"){
             this.playerTurn = "O";
+            gameHeader.innerHTML = this.oPlayer + "'s Turn"
         }
         else {
             this.playerTurn = "X";
+            gameHeader.innerHTML = this.xPlayer + "'s Turn"
         }
-        gameHeader.innerHTML = this.playerTurn + " Player's Turn"
+        
         fillDisplay(this);
         checkWin();
     }
 
     const checkWin = function () {
         //check rows
+        console.log(this);
         if (boardList[0] != "" && boardList[0] == boardList[1] && boardList[1] == boardList[2]){
-            gameHeader.innerHTML =  boardList[0] + " Player Wins!";
+            if (boardList[0] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
         else if (boardList[3] != "" && boardList[3] == boardList[4] && boardList[4] == boardList[5]){
-            gameHeader.innerHTML =  boardList[3] + " Player Wins!";
+            if (boardList[3] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
         else if (boardList[6] != "" && boardList[6] == boardList[7] && boardList[7] == boardList[8]){
-            gameHeader.innerHTML =  boardList[6] + " Player Wins!";
+            if (boardList[6] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
         
         //check cols
         else if (boardList[0] != "" && boardList[0] == boardList[3] && boardList[3] == boardList[6]){
-            gameHeader.innerHTML =  boardList[0] + " Player Wins!";
+            if (boardList[0] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
         else if (boardList[1] != "" && boardList[1] == boardList[4] && boardList[4] == boardList[7]){
-            gameHeader.innerHTML =  boardList[1] + " Player Wins!";
+            if (boardList[1] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
         else if (boardList[2] != "" && boardList[2] == boardList[5] && boardList[5] == boardList[8]){
-            gameHeader.innerHTML =  boardList[2] + " Player Wins!";
+            if (boardList[2] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
 
         //check diagonals
         else if (boardList[0] != "" && boardList[0] == boardList[4] && boardList[4] == boardList[8]){
-            gameHeader.innerHTML =  boardList[0] + " Player Wins!";
+            if (boardList[0] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
         else if (boardList[2] != "" && boardList[2] == boardList[4] && boardList[4] == boardList[6]) {
-            gameHeader.innerHTML =  boardList[2] + " Player Wins!";
+            if (boardList[2] == "X") {
+                gameHeader.innerHTML =  xPlayer + " Wins!";
+            }
+            else {
+                gameHeader.innerHTML =  oPlayer + " Wins!";
+            }
             displayWin(this.boardList);
         }
 
@@ -64,7 +110,7 @@ function createGameBoard () {
         }
     }
 
-    return { boardList, playerTurn, nextTurn};
+    return { boardList, playerTurn, xPlayer, oPlayer, nextTurn};
 }
 
 function fillDisplay (board) {
@@ -108,5 +154,26 @@ function spaceClick () {
     board.nextTurn()
 }
 
-let board = createGameBoard();
-fillDisplay(board);
+let board;
+
+function startGame () {
+    const xName = document.querySelector("#x-player").value;
+    const oName = document.querySelector("#o-player").value;
+    board = createGameBoard(xName, oName);
+    fillDisplay(board);
+    document.querySelector("#game-prompt").style.display = 'none';
+    gameWrapper.style.display = "grid"
+    gameHeader.innerHTML = xName + "'s Turn"
+    gameHeader.style.display = "block"
+    document.querySelector("#restart-btn").style.display = "block";
+    document.querySelector("#x-player").value = "";
+    document.querySelector("#o-player").value = "";
+}
+
+function restartGame () {
+    board = createGameBoard(board.xPlayer, board.oPlayer);
+    fillDisplay(board);
+    gameHeader.innerHTML = board.xPlayer + "'s Turn"
+}
+//let board = createGameBoard();
+//fillDisplay(board);
