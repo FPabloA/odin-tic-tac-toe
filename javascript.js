e6347ca66ebb56272399709bf349c1602bed09bc
FPabloA <pabloayala@protonmail.com>
2025-07-29 11:55:08 -0500

    finished implementing tic tac toe

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const gameWrapper = document.querySelector('#game-display');
+const gameHeader = document.querySelector('#game-header');
+
 function createGameBoard () {
     let boardList = ["", "", "","", "", "","", "", ""];
     let turn = 0;
@@ -5,52 +8,105 @@ function createGameBoard () {
 
     const nextTurn = function () {
         turn++;
-        if (playerTurn == "X"){
-            playerTurn = "O";
+        if (this.playerTurn == "X"){
+            this.playerTurn = "O";
         }
         else {
-            playerTurn = "X";
+            this.playerTurn = "X";
         }
+        gameHeader.innerHTML = this.playerTurn + " Player's Turn"
+        fillDisplay(this);
+        checkWin();
     }
 
     const checkWin = function () {
         //check rows
-        if (boardList[0] == boardList[1] && boardList[1] == boardList[2]){
-            return boardList[0];
+        if (boardList[0] != "" && boardList[0] == boardList[1] && boardList[1] == boardList[2]){
+            gameHeader.innerHTML =  boardList[0] + " Player Wins!";
+            displayWin(this.boardList);
         }
-        else if (boardList[3] == boardList[4] && boardList[4] == boardList[5]){
-            return boardList[3];
+        else if (boardList[3] != "" && boardList[3] == boardList[4] && boardList[4] == boardList[5]){
+            gameHeader.innerHTML =  boardList[3] + " Player Wins!";
+            displayWin(this.boardList);
         }
-        else if (boardList[6] == boardList[7] && boardList[7] == boardList[8]){
-            return boardList[6];
+        else if (boardList[6] != "" && boardList[6] == boardList[7] && boardList[7] == boardList[8]){
+            gameHeader.innerHTML =  boardList[6] + " Player Wins!";
+            displayWin(this.boardList);
         }
         
         //check cols
-        else if (boardList[0] == boardList[3] && boardList[3] == boardList[6]){
-            return boardList[0];
+        else if (boardList[0] != "" && boardList[0] == boardList[3] && boardList[3] == boardList[6]){
+            gameHeader.innerHTML =  boardList[0] + " Player Wins!";
+            displayWin(this.boardList);
         }
-        else if (boardList[1] == boardList[4] && boardList[4] == boardList[7]){
-            return boardList[0];
+        else if (boardList[1] != "" && boardList[1] == boardList[4] && boardList[4] == boardList[7]){
+            gameHeader.innerHTML =  boardList[1] + " Player Wins!";
+            displayWin(this.boardList);
         }
-        else if (boardList[2] == boardList[5] && boardList[5] == boardList[8]){
-            return boardList[0];
+        else if (boardList[2] != "" && boardList[2] == boardList[5] && boardList[5] == boardList[8]){
+            gameHeader.innerHTML =  boardList[2] + " Player Wins!";
+            displayWin(this.boardList);
         }
 
         //check diagonals
-        else if (boardList[0] == boardList[4] && boardList[4] == boardList[8]){
-            return boardList[0];
+        else if (boardList[0] != "" && boardList[0] == boardList[4] && boardList[4] == boardList[8]){
+            gameHeader.innerHTML =  boardList[0] + " Player Wins!";
+            displayWin(this.boardList);
         }
-        else if (boardList[2] == boardList[4] && boardList[4] == boardList[6]) {
-            return boardList[2];
+        else if (boardList[2] != "" && boardList[2] == boardList[4] && boardList[4] == boardList[6]) {
+            gameHeader.innerHTML =  boardList[2] + " Player Wins!";
+            displayWin(this.boardList);
         }
 
         //check if tied
         else if (turn == 9){
-            return "tie";
-        }
-        else {
-            return "";
+            gameHeader.innerHTML =  "Tied!";
         }
     }
-    return { boardList, playerTurn, nextTurn, checkWin };
+
+    return { boardList, playerTurn, nextTurn};
 }
+
+function fillDisplay (board) {
+    while (gameWrapper.firstChild) {
+        gameWrapper.removeChild(gameWrapper.firstChild);
+    }
+
+    for (i = 0; i < board.boardList.length; i++){
+        const newSpace = document.createElement("div");
+        newSpace.id = i;
+        newSpace.className = "game-space"
+        newSpace.innerHTML = board.boardList[i];
+
+        if (board.boardList[i] == ""){
+            newSpace.onclick = spaceClick;
+        }
+        
+        gameWrapper.appendChild(newSpace);
+    }
+}
+
+function displayWin (boardList) {
+    while (gameWrapper.firstChild) {
+        gameWrapper.removeChild(gameWrapper.firstChild);
+    }
+
+    for (i = 0; i < board.boardList.length; i++){
+        const newSpace = document.createElement("div");
+        newSpace.id = i;
+        newSpace.className = "game-space"
+        newSpace.innerHTML = board.boardList[i];
+        
+        gameWrapper.appendChild(newSpace);
+    }
+}
+
+function spaceClick () {
+    // console.log(this.id);
+    // console.log(board.playerTurn);
+    board.boardList[this.id] = board.playerTurn;
+    board.nextTurn()
+}
+
+let board = createGameBoard();
+fillDisplay(board);
